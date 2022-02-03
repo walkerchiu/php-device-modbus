96a4bd9c2852c2fe88f08d38b8a193cf316c04c2
walkerchiu <chenjen.chiou@gmail.com>
2022-02-03 08:15:50 +0800

    Initial commit

---
+++ src/database/factories/AddressFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory  $factory */
+
+use Faker\Generator as Faker;
+use WalkerChiu\Core\Models\Constants\DataType;
+use WalkerChiu\DeviceModbus\Models\Entities\Address;
+use WalkerChiu\DeviceModbus\Models\Entities\AddressLang;
+
+$factory->define(Address::class, function (Faker $faker) {
+    return [
+        'serial'     => $faker->isbn10,
+        'identifier' => $faker->slug,
+        'order'      => $faker->randomNumber,
+        'data_type'  => $faker->randomElement(DataType::getCodes())
+    ];
+});
+
+$factory->define(AddressLang::class, function (Faker $faker) {
+    return [
+        'code'  => $faker->locale,
+        'key'   => $faker->randomElement(['name', 'description']),
+        'value' => $faker->sentence
+    ];
+});
---
+++ src/database/factories/SettingStateFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory  $factory */
+
+use Faker\Generator as Faker;
+use WalkerChiu\DeviceModbus\Models\Entities\SettingState;
+use WalkerChiu\DeviceModbus\Models\Entities\SettingStateLang;
+
+$factory->define(SettingState::class, function (Faker $faker) {
+    return [
+        'serial'     => $faker->isbn10,
+        'identifier' => $faker->slug,
+        'mean'       => $faker->slug
+    ];
+});
+
+$factory->define(SettingStateLang::class, function (Faker $faker) {
+    return [
+        'code'  => $faker->locale,
+        'key'   => $faker->randomElement(['name', 'description']),
+        'value' => $faker->sentence
+    ];
+});
